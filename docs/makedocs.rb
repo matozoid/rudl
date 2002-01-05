22b4bddb47eaf2c2ea8e17ab70ca3528a96c5f27
tsuihark <tsuihark>
2002-01-05 22:01:28 +0000

    Made makedocs copy index and howto too.

--- docs/makedocs.rb
+++ docs/makedocs.rb
@@ -1,5 +1,7 @@
 #!/usr/local/bin/ruby
 
+require 'ftools'
+
 puts 'This creates the RUDL documentation.'
 puts 'You can make it create documentation in another directory by giving that directory as an argument to this program.'
 puts 'This will also create some fake 0 bytes documentation for files that had no documentation in them.'
@@ -14,5 +16,9 @@ sourcefiles.each {|source|
 	basename=File.basename(source, '.c')
 	destination="#{prefix}/#{basename}.html"
 	puts basename
-	`rd2 #{source} > #{destination}`
+	#`rd2 #{source} > #{destination}`
 }
+
+File.copy 'index.html', prefix
+File.copy 'howto.html', prefix
+
