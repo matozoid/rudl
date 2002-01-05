81a431eecdc5c8a141a10366cb8510ae7c5f1589
tsuihark <tsuihark>
2002-01-05 20:57:22 +0000

    Initial revision

---
+++ docs/makedocs.rb
@@ -0,0 +1,18 @@
+#!/usr/local/bin/ruby
+
+puts 'This creates the RUDL documentation.'
+puts 'You can make it create documentation in another directory by giving that directory as an argument to this program.'
+puts 'This will also create some fake 0 bytes documentation for files that had no documentation in them.'
+
+prefix='.'
+
+prefix=ARGV[0] if ARGV.length>0
+
+sourcefiles=Dir['../rudl*.c']
+
+sourcefiles.each {|source|
+	basename=File.basename(source, '.c')
+	destination="#{prefix}/#{basename}.html"
+	puts basename
+	`rd2 #{source} > #{destination}`
+}
