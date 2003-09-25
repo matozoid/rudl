d7608f7e4bbbcb366bc314830decbdd959a88b38
rennex <rennex>
2003-09-25 22:38:06 +0000

    Merged the old versions into an "include, then exclude" version. Added option to only list files

--- package.rb
+++ package.rb
@@ -1,8 +1,7 @@
 # package.rb
 # ----------
-# This version selects files by exclusion!
 
-# todo: don't exclude anything from samples/ ?
+# todo: tar.gz for sources
 
 # by Rennex on 24 Sep 2003
 
@@ -22,8 +21,16 @@ puts
 #--------------
 
 def main
+    what = ARGV[0]
+
+    # if the first argument is "list", only list the files to be zipped
+    if what =~ /list/i
+        what = ARGV[1]
+        $list = true
+    end
+
     # What shall we package?
-    case ARGV[0]
+    case what
         when /source|src/i
             zipit("source")
 
@@ -39,68 +46,84 @@ def main
 end
 
 
-# create either the source or setup archive
-def zipit(what)
-
-    # files to always exclude
-    ignoretypes = [ /\.(obj|def|exp|pdb|lib)$/i, /^makefile$/i, /^mkmf.log$/i,
-                    /^package.*\.rb$/i, /^#$destdir\//i ]
-
-    # list everything
-    files = Dir["**/*"]
-
-    # exclude directory names and dirs named "CVS" (courtesy of WinCVS)
+# recursively list the contents of the dir, ignoring the
+# dir names and dirs named "CVS"
+def lsdir(name)
+    if name
+        name += "/" unless name =~ /\/$/
+        name += "**/*"
+    else
+        name = "**/*"
+    end
+    files = Dir[name]
     files.reject! {|f|
         File.stat(f).directory? or f =~ %r{(^|/)CVS(/|$)}i
     }
+    return files
+end
 
-    # helper method to exclude the files that we never want to include
-    def files.reject_list!(ary)
+
+# helper method to exclude the files that we don't want to include
+class Array
+    def reject_list!(ary)
         self.reject! {|f|
-            # got to use this trick because "return true" exits the whole method
+            # got to use this break trick because "return true" exits the whole method
             found = ary.each do |re|
-                break "exclude" if f =~ re
+                if re.is_a? Regexp
+                    # why does Regexp.new(re, Regexp::IGNORECASE) ignore my flags?
+                    break "exclude" if f.downcase =~ re
+                else
+                    # compare strings as case-insensitive
+                    break "exclude" if f.downcase == re.downcase
+                end
             end
             found == "exclude"
         }
     end
+end
 
-    # exclude the defaults
-    files.reject_list!(ignoretypes)
 
-	# which archive are we creating?
-	case what
-		when "source"
-		files.reject_list! [
-				/^dll\//i,
-				/^lib\//i,
-				/\.dll$/i,
-				/^RUDL.so$/i 
-		]
-	
-	when "setup"
-		files.reject_list! [ 
-				/^include\//i, 
-				/^lib\//i, 
-				/\.(c|h)$/i,
-				/^configure$/i, 
-				/^make.bat$/i, 
-				/^extconf.rb$/i 
-		]
+# create either the source or setup archive
+def zipit(what)
+
+    # which archive are we creating?
+    case what
+        when "source"
+            files = lsdir("docs") + lsdir("include") +
+                    lsdir("samples") + lsdir("utility") +
+                    Dir["*.{c,h,txt,rb}"] << "configure" << "make.bat"
+
+        when "setup"
+            files = lsdir("dll") + lsdir("docs") +
+                    lsdir("samples") + lsdir("utility") +
+                    Dir["*.txt"] << "RUDL.so" << "install-on-windows.rb"
 
         else
             raise "Oh God please no!"
     end
 
-    outfile = "#$destdir/rudl-#$version-#{what}.zip"
+    files.reject_list! [
+        /\.(obj|def|exp|pdb|lib)$/,
+        "makefile",
+        "mkmf.log",
+        /^package.*\.rb$/,
+        $destdir,
+        /^samples\/work\//,
+    ]
 
-    Dir.mkdir($destdir) unless File.exists?($destdir)
-    File.delete(outfile) if File.exists?(outfile)
+    if not $list
+        outfile = "#$destdir/rudl-#$version-#{what}.zip"
 
-    # I hope the command line doesn't get truncated :)
-    puts "Zipping #{outfile}..."
-    files.each do |filename|
-    	system("zip -9 -q #{outfile} \"#{filename}\"")
+        Dir.mkdir($destdir) unless File.exists?($destdir)
+        File.delete(outfile) if File.exists?(outfile)
+
+        # zip em up! one at a time, to avoid truncated command lines
+        puts "Zipping #{outfile}..."
+        files.each do |filename|
+            system("zip -9 -q #{outfile} \"#{filename}\"")
+        end
+    else
+        puts files
     end
 end
 
