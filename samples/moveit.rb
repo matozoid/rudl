26b22415d22467b51219efbb8b5400d895896f89
tsuihark <tsuihark>
2003-09-26 18:10:54 +0000

    Some test scripts from when Nauglin still worked on RUDL

---
+++ samples/moveit.rb
@@ -0,0 +1,169 @@
+=begin
+---------------------------------------------------------------------
+This is a porting from the pygame script to RUDL. The original pygame 
+code is at the end. I've found the RUDL version running at similar speed 
+than the pygame one.
+
+The main difference is that pygame seems to accept a Rect as a 
+coordinate pair, and it also stores in the Rect object the right and 
+bottom coordinates besides the left, top, with and height. This has 
+forced me to do the GameObject.move method and the background 
+restoring blit a bit different, as I must to calculate the rigth coordinate 
+for each sprite on the fly every loop. I first tried to make the @pos 
+instance variable look like a [left, top, width, height] array instead of 
+a [left, top] coordinate pair, but the background restoring blit become 
+a bit slower that way, becouse you have to get a slice from the @pos 
+rect to obtain the position, two times. It seems to be faster adding two 
+arrays than slicing one twice ?? 
+
+I'm new to Ruby and RUDL, and indeed I'm new to OOP, so maybe 
+someone more used to Ruby could try to improve all of this. If so, please 
+let us see the resultant code.
+
+NOTE: You'll need a 690x530x16bpp BMP for the script to work.
+
+   [- nauglin -]
+---------------------------------------------------------------------
+=end
+
+#-- require and include everything
+require 'RUDL'; include RUDL; include Constant
+
+#-- our game object class
+class GameObject
+	attr_reader :pos, :image
+	def initialize( image, height, speed )
+		@speed = speed
+		@image = image
+		@pos = [ 0, height ]
+		 #-- comment the above line and uncomment the next to try 
+		 #-- the other way
+		#@pos = [ 0, height, image.w, image.h ]
+	end
+	def move
+		@pos[0] = @pos[0] + @speed
+		if  @pos[0] + @image.w > 600
+			@pos[0] = 0
+		end
+	end
+end
+
+
+#-- quick function to load an image
+def load_image(name)
+	return Surface.load_new( name ).convert()
+end
+
+
+#-- here's the full code
+def main
+	screen = DisplaySurface.new( [640, 480] )
+	
+	player = load_image('media/player1.gif')
+	background = load_image('media/liquid.gif')  #-- YOU NEED SOME FILE HERE
+	screen.blit( background, [0, 0] )
+	
+	objects = []
+	for x in 0...10
+		objects << GameObject.new( player, x*40, x )
+	end
+
+	while 1
+		for event in EventQueue.get
+			return if [QuitEvent, KeyDownEvent].include?( event.class )
+		end
+
+		for o in objects
+			screen.blit( background, o.pos,  o.pos + o.image.size )
+			 #-- comment the above line and uncomment the next to try 
+			 #-- the other way
+			#screen.blit( background, o.pos[0,2], o.pos )
+		end
+		for o in objects
+			o.move
+			screen.blit( o.image, o.pos )
+			 #-- comment the above line and uncomment the next to try 
+			 #-- the other way
+			#screen.blit( o.image, o.pos[0,2] )
+		end
+
+        screen.update
+	end
+end
+
+
+
+main
+
+
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+
+__END__
+
+#!/usr/bin/env python
+
+"""
+This is the full and final example from the Pygame Tutorial,
+"How Do I Make It Move". It creates 10 objects and animates
+them on the screen.
+
+Note it's a bit scant on error checking, but it's easy to read. :]
+Fortunately, this is python, and we needn't wrestle with a pile of
+error codes.
+"""
+
+
+#import everything
+import os, pygame
+from pygame.locals import *
+
+#our game object class
+class GameObject:
+    def __init__(self, image, height, speed):
+        self.speed = speed
+        self.image = image
+        self.pos = image.get_rect().move(0, height)
+    def move(self):
+        self.pos = self.pos.move(self.speed, 0)
+        if self.pos.right > 600:
+            self.pos.left = 0
+
+
+#quick function to load an image
+def load_image(name):
+    path = os.path.join('data', name)
+    return pygame.image.load(path).convert()
+
+
+#here's the full code
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+
+    player = load_image('player1.gif')
+    background = load_image('liquid.bmp')
+    screen.blit(background, (0, 0))
+
+    objects = []
+    for x in range(10):
+        o = GameObject(player, x*40, x)
+        objects.append(o)
+
+    while 1:
+        for event in pygame.event.get():
+            if event.type in (QUIT, KEYDOWN):
+                return
+
+        for o in objects:
+            screen.blit(background, o.pos, o.pos)
+        for o in objects:
+            o.move()
+            screen.blit(o.image, o.pos)
+
+        pygame.display.update()
+
+
+
+if __name__ == '__main__': main()
---
+++ samples/movesp.rb
@@ -0,0 +1,261 @@
+=begin
+---------------------------------------------------------------------
+This is a porting from the Ohai's script to RUDL. The Ohai's original 
+code is at the end. I've tried this to be the most similar as posible 
+to the original, but RUDL and Ruby/SDL are different approaches to 
+SDL, so...   [-nauglin-]
+---------------------------------------------------------------------
+=end
+
+
+require 'RUDL'; include RUDL; include Constant
+
+screen = DisplaySurface.new( [640,480], SWSURFACE, 16 )
+
+image = Surface.load_new( 'media/icon.gif' )
+image.set_colorkey( image[0,0], RLEACCEL )
+
+$image = image.convert     #-- I believe RUDL can do this directly on 
+                           #-- the surface itself, but I'm not sure. 
+                           #-- Anyway, this way the comparing 
+                           #-- should be more reliable.
+
+srand 10     #-- This is for always having the same random numbers, 
+             #-- so that speed comparing makes any sense.
+
+class Sprite
+  def initialize
+    @x=rand(640)
+    @y=rand(480)
+    @dx=rand(11)-5
+    @dy=rand(11)-5
+  end
+  
+  def move
+    @x += @dx
+    if @x >= 640 then
+      @dx *= -1
+      @x = 639
+    end
+    if @x < 0 then
+      @dx *= -1
+      @x = 0
+    end
+    @y += @dy           #-- I wonder why Ohai does this twice...
+    if @y >= 480 then
+      @dy *= -1
+      @y = 479
+    end
+    @y += @dy           #-- ...but only in the Y axis ??
+    if @y < 0 then
+      @dy *= -1
+      @y = 0
+    end
+  end
+  
+  def draw(screen)
+    screen.blit( $image, [@x,@y], [0,0,32,32] )
+  end
+  
+end
+
+sprites = []
+for i in 1..5
+  sprites << Sprite.new
+end
+
+class MovableSp
+  def initialize()
+    @ud=@lr=0;
+  end
+  
+  def move()
+    @ud=@lr=0;
+    @lr=-1 if Key.pressed?[K_h] or Key.pressed?[K_LEFT]
+    @lr=1  if Key.pressed?[K_l] or Key.pressed?[K_RIGHT]
+    @ud=1  if Key.pressed?[K_j] or Key.pressed?[K_DOWN]
+    @ud=-1 if Key.pressed?[K_k] or Key.pressed?[K_UP]
+  end
+  
+  def draw(screen)
+    screen.blit( $image, [300+@lr*50,200+@ud*50], [0,0,32,32] )
+    screen.flip # comment this out for comparing speed
+  end
+end
+
+sprites << MovableSp.new
+
+event = EventQueue.pump   #-- Just to put something with sense where 
+                          #-- Ohai did  SDL::Event.new
+
+fps = 0           #-- These two are for calculating the frames per 
+t = Timer.ticks   #-- second. Not in the original.
+
+while true   #-- Start the main loop
+
+   #-- Next is a variable assignment not so evident in the Ohai's
+   #-- original. Of course, Ruby/SDL should also do this assignment
+   #-- internally anyway. Maybe this is more time consuming, as is
+   #-- Ruby instead inner C, but it doesn't seem to slow things visibly.
+   #
+   #-- I still think this could be a bit more clear:
+   #
+   #-- if event = EventQueue.poll
+   #--   break if event.class == QuitEvent
+   #--   break if event.class == KeyDownEvent and event.key == K_ESCAPE
+   #-- end
+   #
+   #-- But I'm trying to duplicate Ohai's code as much as I can.
+
+  if  event = EventQueue.poll
+    if event.class == QuitEvent
+      break
+    end
+    if event.class == KeyDownEvent
+      break if event.key == K_ESCAPE
+    end
+  end
+  
+   #-- This would work the same in RUDL without the Rect.
+  screen.fill( 0, [0,0,640,480] )
+  
+   #-- This is to keep the keyboard queue up to date.
+   #-- I hope it being similar to Ohai's  Key.scan
+  EventQueue.pump            
+  
+  sprites.each {|i|
+    i.move
+    i.draw(screen)
+  }
+
+   #-- This would work the same in RUDL without the arg.
+  screen.update( [ [0,0,0,0] ] )
+
+   #-- This is for counting the frames.
+  fps += 1
+
+end
+
+#-- These are not from the original. They are the fps calculation.
+t = Timer.ticks - t
+screen.set_caption "#{fps/(t/1000.0)} fps"
+
+#-- Just a wait before quitting...
+true while EventQueue.poll
+true until [QuitEvent, KeyDownEvent].include? EventQueue.poll.class
+
+
+
+__END__
+
+=begin
+---------------------------------------------------------------------
+This is the Ohai's original code.  I've put some minnor changes for 
+testing speed, but they're commented. Just uncomment'em to get fps 
+calculation.   [-nauglin-]
+---------------------------------------------------------------------
+=end
+
+
+require 'sdl'
+
+SDL.init( SDL::INIT_VIDEO )
+
+screen = SDL::setVideoMode(640,480,16,SDL::SWSURFACE)
+
+image = SDL::Surface.loadBMP("icon.bmp")
+image.setColorKey( SDL::SRCCOLORKEY || SDL::RLEACCEL ,0)
+$image = image.displayFormat
+
+#srand 10   #-- Uncomment to always test with the same randomnes.
+
+class Sprite
+  def initialize
+    @x=rand(640)
+    @y=rand(480)
+    @dx=rand(11)-5
+    @dy=rand(11)-5
+  end
+  
+  def move
+    @x += @dx
+    if @x >= 640 then
+      @dx *= -1
+      @x = 639
+    end
+    if @x < 0 then
+      @dx *= -1
+      @x = 0
+    end
+    @y += @dy
+    if @y >= 480 then
+      @dy *= -1
+      @y = 479
+    end
+    @y += @dy
+    if @y < 0 then
+      @dy *= -1
+      @y = 0
+    end
+  end
+  
+  def draw(screen)
+    SDL.blitSurface($image,0,0,32,32,screen,@x,@y)
+  end
+  
+end
+
+sprites = []
+for i in 1..5
+  sprites << Sprite.new
+end
+
+class MovableSp
+  def initialize()
+    @ud=@lr=0;
+  end
+  
+  def move()
+    @ud=@lr=0;
+    @lr=-1 if SDL::Key.press?(SDL::Key::H) or SDL::Key.press?(SDL::Key::LEFT)
+    @lr=1  if SDL::Key.press?(SDL::Key::L) or SDL::Key.press?(SDL::Key::RIGHT)
+    @ud=1  if SDL::Key.press?(SDL::Key::J) or SDL::Key.press?(SDL::Key::DOWN)
+    @ud=-1 if SDL::Key.press?(SDL::Key::K) or SDL::Key.press?(SDL::Key::UP)
+  end
+  
+  def draw(screen)
+    SDL.blitSurface($image,0,0,32,32,screen,300+@lr*50,200+@ud*50)
+  end
+end
+
+sprites << MovableSp.new
+
+event=SDL::Event.new
+
+#fps = 0           #-- Uncoment to test frames per second.
+#t = SDL.getTicks  #-- This one too.
+
+while true 
+  if  event.poll != 0 then
+    if event.class==SDL::Event::QUIT then
+      break
+    end
+    if event.class==SDL::Event::KEYDOWN then
+      break if event.keySym==SDL::Key::ESCAPE
+    end
+  end
+  screen.fillRect(0,0,640,480,0)
+  SDL::Key.scan
+  
+  sprites.each {|i|
+    i.move
+    i.draw(screen)
+  }
+  screen.updateRect(0,0,0,0)
+  #fps += 1  #-- Uncomment to count frames.
+end
+
+#-- Uncomment next lines to get fps result.
+#t = SDL.getTicks - t
+#SDL::WM::setCaption "#{fps/(t/1000.0)} fps", $0
+#sleep 3
