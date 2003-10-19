4c2cb0a0b27e67437233d6cb2fea6d32fa33157d
rennex <rennex>
2003-10-19 10:56:00 +0000

    Rewrote a lot

--- samples/events.rb
+++ samples/events.rb
@@ -1,38 +1,50 @@
+
+# this example could use some more rewriting, and the guidelines
+# presented here belong in the main documentation :)
+
+
 require 'RUDL'
 # we would usually "include RUDL" and "include Constant" to avoid
-# having to write RUDL:: in front of RUDL classes.
+# having to write RUDL:: or RUDL::Constant:: everywhere
 
-puts
-puts "Events example"
-puts "--------------"
-puts
-puts "Read the source to see a normal RUDL eventloop."
-puts
-puts "There are two ways of handling input: polling or waiting."
-puts "Polling means checking if events have occurred, when you feel like it."
-puts "Waiting means that the program is paused until some event occurs."
-puts
-puts "For example: you could poll every now and then whether a key is pressed,"
-puts "and in the meanwhile keep running your animation on screen."
-puts "Or you can wait for any event to occur if there's nothing else to do."
-puts
-puts "You will see a window appearing. It will print all events that it gets to"
-puts "the console."
+puts <<_end_
+Events example
+--------------
+
+Read the source to see a normal RUDL input handler loop.
+
+There are two ways of handling input: events or polling.
+Events means monitoring the changes in the keyboard or mouse's state.
+Polling means reading the state of keys or the mouse, when you need it.
+
+Events are the preferred method, because when using polling you can miss
+keypresses or mouse clicks completely if you don't poll often enough,
+and the slower the computer is the greater the risk. It is very annoying
+to the user and makes the application seem unresponsive. When using
+events, it is impossible or at least very difficult to miss clicks and
+keypresses.
+
+This program will now print to the console all events that the window
+receives.
+_end_
 
 # There has to be a RUDL::DisplaySurface for most events to fire
-RUDL::DisplaySurface.new([150,100])
+RUDL::DisplaySurface.new([150,100], RUDL::Constant::RESIZABLE)
 
 # This outer loop simulates your game loop.
 # Event handling is done in a loop within the game loop.
-while true
+loop do
 
-    # RUDL::EventQueue is the main event system. It contains a queue with events that
-    # have happened. You can get events from it in several ways. EventQueue.poll gets
-    # the next unprocessed event or nil if there aren't any. EventQueue.get returns
-    # an array with all the unprocessed events.
+    # RUDL::EventQueue is the main event system. It contains a queue with events
+    # that have happened. You can get events from it in several ways. EventQueue.get
+    # returns an array with all the unprocessed events. EventQueue.wait pauses the
+    # program until an event occurs, and returns that event. EventQueue.poll gets
+    # the next unprocessed event or nil if there aren't any. (Note: don't confuse
+    # this with the polling mentioned above. Polling for events is preferable to
+    # polling for keyboard/mouse state. Unless you busy loop that is, see below.)
 
-    # Normally, you'll want to process all events after you've drawn a frame,
-    # and then draw the next frame.
+    # Normally, you'll want to process all events, and then proceed to draw the
+    # next frame.
 
     RUDL::EventQueue.get.each do |event|
 
@@ -61,6 +73,10 @@ while true
 
     end
 
+    # Now that we've processed the events, we should draw the next frame of animation
+    # or whatever the game is doing.
+
+
     # Important! Do not busy poll! This means that if there aren't any events you just
     # check the events again without any delay. This WILL use 100% CPU time, even when
     # the program is idle! But even a delay of 1 millisecond is enough to reduce CPU
@@ -70,6 +86,10 @@ while true
     # 20 times a second. (1000/50 = 20)
 
     RUDL::Timer.delay(50)
+
+    # If this wasn't a simulation of a game's event loop, we'd use EventQueue.wait
+    # instead of EventQueue.get since we have nothing to do when there are no events.
+    # Then the delay wouldn't be needed.
 
     # If you're running full screen, DisplaySurface#flip will wait for the next screen
     # refresh (vertical blanking period); this will limit your loop to about 85 times
