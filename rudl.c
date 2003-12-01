96cb7ba5dfaf3689aef022cfd85e9010e1b438e4
rennex <rennex>
2003-12-01 22:18:14 +0000

    Fixed some typos in docs

--- rudl.c
+++ rudl.c
@@ -13,30 +13,30 @@
 
 // For getting versions:
 #ifdef HAVE_SDL_MIXER_H
-	#include "SDL_mixer.h"
-	#ifndef MIX_MAJOR_VERSION
-		#define MIX_MAJOR_VERSION 0
-		#define MIX_MINOR_VERSION 0
-	#endif
+    #include "SDL_mixer.h"
+    #ifndef MIX_MAJOR_VERSION
+        #define MIX_MAJOR_VERSION 0
+        #define MIX_MINOR_VERSION 0
+    #endif
 #endif
 #ifdef HAVE_SMPEG_SMPEG_H
-	#include "smpeg/smpeg.h"
+    #include "smpeg/smpeg.h"
 #endif
 #ifdef HAVE_SDL_GFXPRIMITIVES_H
-	#include "SDL_gfxPrimitives.h"
+    #include "SDL_gfxPrimitives.h"
 #endif
 #ifdef HAVE_SDL_FRAMERATE_H
-	#include "SDL_framerate.h"
+    #include "SDL_framerate.h"
 #endif
 #ifdef HAVE_SDL_IMAGEFILTER_H
-	#include "SDL_imageFilter.h"
+    #include "SDL_imageFilter.h"
 #endif
 #ifdef HAVE_SDL_ROTOZOOM_H
-	#include "SDL_rotozoom.h"
+    #include "SDL_rotozoom.h"
 #endif
 
 #ifdef HAVE_SDL_NET_H
-	#include "SDL_net.h"
+    #include "SDL_net.h"
 #endif
 //
 
@@ -62,54 +62,54 @@ QDGlobals qd;
 ///////////////////////////////// "CORE"
 void initSDL()
 {
-	if(!sDLInitWasCalled){
+    if(!sDLInitWasCalled){
 #ifdef MS_WIN32
-		SDL_RegisterApp("RUDL window", 0, GetModuleHandle(NULL));
+        SDL_RegisterApp("RUDL window", 0, GetModuleHandle(NULL));
 #endif
 #if defined(macintosh)
 #if !TARGET_API_MAC_CARBON
-		SDL_InitQuickDraw(&qd);
+        SDL_InitQuickDraw(&qd);
 #endif
-#endif		
-		rb_eval_string("Kernel.at_exit {RUDL.at_exit}");
-		sDLInitWasCalled=true;
-	}
+#endif
+        rb_eval_string("Kernel.at_exit {RUDL.at_exit}");
+        sDLInitWasCalled=true;
+    }
 }
 
 static VALUE RUDL_at_exit(VALUE obj)
 {
-	DEBUG_S("Reached RUDL_at_exit");
+    DEBUG_S("Reached RUDL_at_exit");
 #ifdef HAVE_SDL_NET_H
-	quitNet();
+    quitNet();
 #endif
-	quitJoystick();
+    quitJoystick();
 #ifdef HAVE_SDL_MIXER_H
-	quitAudio();
+    quitAudio();
 #endif
-	quitVideo();
-	quitTTF();
+    quitVideo();
+    quitTTF();
 
-	DEBUG_S("Quitting the rest of it");
-	SDL_Quit();
-	return Qnil;
+    DEBUG_S("Quitting the rest of it");
+    SDL_Quit();
+    return Qnil;
 }
 
 __inline__ Uint32 PARAMETER2FLAGS(VALUE flagsArg)
 {
-	Uint32 flags=0;
-	int i;
+    Uint32 flags=0;
+    int i;
 
-	VALUE tmp;
+    VALUE tmp;
 
-	if(rb_obj_is_kind_of(flagsArg, rb_cArray)){
-		for(i=0; i<RARRAY(flagsArg)->len; i++){
-			tmp=rb_ary_entry(flagsArg, i);
-			flags|=NUM2UINT(tmp);
-		}
-	}else{
-		flags=NUM2UINT(flagsArg);
-	}
-	return flags;
+    if(rb_obj_is_kind_of(flagsArg, rb_cArray)){
+        for(i=0; i<RARRAY(flagsArg)->len; i++){
+            tmp=rb_ary_entry(flagsArg, i);
+            flags|=NUM2UINT(tmp);
+        }
+    }else{
+        flags=NUM2UINT(flagsArg);
+    }
+    return flags;
 }
 
 
@@ -125,90 +125,90 @@ __inline__ VALUE rb_range_last(VALUE obj)
 
 VALUE RUDL_init(VALUE obj, VALUE flags)
 {
-	initSDL();
-	return UINT2NUM(SDL_Init(PARAMETER2FLAGS(flags)));
+    initSDL();
+    return UINT2NUM(SDL_Init(PARAMETER2FLAGS(flags)));
 }
 
 VALUE RUDL_quit(VALUE obj, VALUE flags)
 {
-	SDL_QuitSubSystem(PARAMETER2FLAGS(flags));
-	return obj;
+    SDL_QuitSubSystem(PARAMETER2FLAGS(flags));
+    return obj;
 }
 
 VALUE RUDL_is_init(VALUE obj, VALUE flags)
 {
-	initSDL();
-	return UINT2NUM(SDL_WasInit(PARAMETER2FLAGS(flags)));
+    initSDL();
+    return UINT2NUM(SDL_WasInit(PARAMETER2FLAGS(flags)));
 }
 
 static VALUE RUDL_versions(VALUE self)
 {
-	char versions[8192]; // Ugh
-	sprintf(versions, "{"
-		"'RUDL'=>RUDL::Version.new(%i,%i,%i),\n"
-		"'SDL'=>RUDL::Version.new(%i,%i,%i),\n"
-		"'BitMask'=>RUDL::Version.new,\n"
+    char versions[8192]; // Ugh
+    sprintf(versions, "{"
+        "'RUDL'=>RUDL::Version.new(%i,%i,%i),\n"
+        "'SDL'=>RUDL::Version.new(%i,%i,%i),\n"
+        "'BitMask'=>RUDL::Version.new,\n"
 #ifdef HAVE_SDL_IMAGE_H
-		"'SDL_image'=>RUDL::Version.new,\n"
+        "'SDL_image'=>RUDL::Version.new,\n"
 #endif
 #ifdef HAVE_SDL_MIXER_H
-		"'SDL_mixer'=>RUDL::Version.new(%i, %i),\n"
+        "'SDL_mixer'=>RUDL::Version.new(%i, %i),\n"
 #endif
 #ifdef HAVE_SDL_TTF_H
-		"'SDL_ttf'=>RUDL::Version.new,\n"
+        "'SDL_ttf'=>RUDL::Version.new,\n"
 #endif
 #ifdef HAVE_SDL_GFXPRIMITIVES_H
-		"'SDL_gfxPrimitives'=>RUDL::Version.new(%i, %i),\n"
+        "'SDL_gfxPrimitives'=>RUDL::Version.new(%i, %i),\n"
 #endif
 #ifdef HAVE_SDL_FRAMERATE_H
-// not used	"'SDL_framerate'=>RUDL::Version.new,\n"
+// not used "'SDL_framerate'=>RUDL::Version.new,\n"
 #endif
 #ifdef HAVE_SDL_IMAGEFILTER_H
-//		"'SDL_imagefilter'=>RUDL::Version.new,\n"
+//      "'SDL_imagefilter'=>RUDL::Version.new,\n"
 #endif
 #ifdef HAVE_SDL_ROTOZOOM_H
-		"'SDL_rotozoom'=>RUDL::Version.new,\n"
+        "'SDL_rotozoom'=>RUDL::Version.new,\n"
 #endif
 #ifdef HAVE_SMPEG_SMPEG_H
-		"'smpeg'=>RUDL::Version.new(%i, %i, %i),\n"
+        "'smpeg'=>RUDL::Version.new(%i, %i, %i),\n"
 #endif
 #ifdef HAVE_SDL_NET_H
-		"'net'=>RUDL::Version.new,\n"
+        "'net'=>RUDL::Version.new,\n"
 #endif
-	"}"
-	,RUDLVERSION_MAJOR, RUDLVERSION_MINOR, RUDLVERSION_PATCH
-	,SDL_MAJOR_VERSION, SDL_MINOR_VERSION, SDL_PATCHLEVEL
-	// no version info (BitMask)
+    "}"
+    ,RUDLVERSION_MAJOR, RUDLVERSION_MINOR, RUDLVERSION_PATCH
+    ,SDL_MAJOR_VERSION, SDL_MINOR_VERSION, SDL_PATCHLEVEL
+    // no version info (BitMask)
 #ifdef HAVE_SDL_IMAGE_H
-	// no version info
+    // no version info
 #endif
 #ifdef HAVE_SDL_MIXER_H
-	,MIX_MAJOR_VERSION ,MIX_MINOR_VERSION
+    ,MIX_MAJOR_VERSION ,MIX_MINOR_VERSION
 #endif
 #ifdef HAVE_SDL_TTF_H
-	// no version info
+    // no version info
 #endif
 #ifdef HAVE_SDL_GFXPRIMITIVES_H
-	,SDL_GFXPRIMITIVES_MAJOR, SDL_GFXPRIMITIVES_MINOR
+    ,SDL_GFXPRIMITIVES_MAJOR, SDL_GFXPRIMITIVES_MINOR
 #endif
 #ifdef HAVE_SDL_FRAMERATE_H
-	// no version info
+    // no version info
 #endif
 #ifdef HAVE_SDL_IMAGEFILTER_H
-	// no version info
+    // no version info
 #endif
 #ifdef HAVE_SDL_ROTOZOOM_H
-	// no version info
+    // no version info
 #endif
 #ifdef HAVE_SMPEG_SMPEG_H
-	,SMPEG_MAJOR_VERSION, SMPEG_MINOR_VERSION, SMPEG_PATCHLEVEL
+    ,SMPEG_MAJOR_VERSION, SMPEG_MINOR_VERSION, SMPEG_PATCHLEVEL
 #endif
 
 #ifdef HAVE_SDL_NET_H
-	// no version info
+    // no version info
 #endif
-	);
-	return rb_eval_string(versions);
+    );
+    return rb_eval_string(versions);
 }
 
 //////////////////////////////////
@@ -221,7 +221,7 @@ Module (({RUDL})) contains all RUDL classes as inner classes.
 It has some class methods of its own too.
 == Class Methods
 --- RUDL.versions
-Returns hash of librarynames with their versions that are supported by RUDL.
+Returns a hash of library names with their versions that are supported by RUDL.
 This list was determined when RUDL was compiled for a certain system,
 and might change when other libraries have been installed or removed
 and RUDL is recompiled.
@@ -251,70 +251,70 @@ INIT_TIMER, INIT_AUDIO, INIT_VIDEO, INIT_CDROM, INIT_JOYSTICK, INIT_NOPARACHUTE,
 
 DECKLSPECKL void Init_RUDL()
 {
-	DEBUG_S("Init_RUDL()");
+    DEBUG_S("Init_RUDL()");
 
-	moduleRUDL=rb_define_module("RUDL");
-	
-	rb_define_singleton_method(moduleRUDL, "at_exit", RUDL_at_exit, 0);
-	rb_define_singleton_method(moduleRUDL, "init_subsystem", RUDL_init, 1);
-	rb_define_singleton_method(moduleRUDL, "quit_subsystem", RUDL_quit, 1);
-	rb_define_singleton_method(moduleRUDL, "is_subsystem_init", RUDL_is_init, 1);
-	rb_define_singleton_method(moduleRUDL, "versions", RUDL_versions, 0);
+    moduleRUDL=rb_define_module("RUDL");
 
-	classSDLError=rb_define_class("SDLError", rb_eStandardError);
-	moduleConstant=rb_define_module_under(moduleRUDL, "Constant");
+    rb_define_singleton_method(moduleRUDL, "at_exit", RUDL_at_exit, 0);
+    rb_define_singleton_method(moduleRUDL, "init_subsystem", RUDL_init, 1);
+    rb_define_singleton_method(moduleRUDL, "quit_subsystem", RUDL_quit, 1);
+    rb_define_singleton_method(moduleRUDL, "is_subsystem_init", RUDL_is_init, 1);
+    rb_define_singleton_method(moduleRUDL, "versions", RUDL_versions, 0);
 
-	classPit=rb_define_class_under(moduleRUDL, "Pit", rb_cObject);
+    classSDLError=rb_define_class("SDLError", rb_eStandardError);
+    moduleConstant=rb_define_module_under(moduleRUDL, "Constant");
 
-	rb_eval_string(
-		"module RUDL\n"
-		"	class Version\n"
-		"		attr_accessor :major, :minor, :patch, :deepest\n"
-		"		def <(v)\n"
-		"			(@major<v.major) or\n"
-		"			(@major==v.major and @minor<v.minor) or\n"
-		"			(@major==v.major and @minor==v.minor and @patch<v.patch) or\n"
-		"			(@major==v.major and @minor==v.minor and @patch==v.patch and @deepest<v.deepest)\n"
-		"		end\n"
-		"		def initialize(major=0, minor=0, patch=0, deepest=0)\n"
-		"			@major=major\n"
-		"			@minor=minor\n"
-		"			@patch=patch\n"
-		"			@deepest=deepest\n"
-		"		end\n"
-		"		def to_s\n"
-		"			\"#{major}.#{minor}.#{patch}.#{deepest}\"\n"
-		"		end\n"
-		"	end\n"
-		"end\n"
-	);
+    classPit=rb_define_class_under(moduleRUDL, "Pit", rb_cObject);
 
-	id_begin=rb_intern("begin");
-	id_end=rb_intern("end");
-	id_new=rb_intern("new");
-	id_clone=rb_intern("clone");
+    rb_eval_string(
+        "module RUDL\n"
+        "   class Version\n"
+        "       attr_accessor :major, :minor, :patch, :deepest\n"
+        "       def <(v)\n"
+        "           (@major<v.major) or\n"
+        "           (@major==v.major and @minor<v.minor) or\n"
+        "           (@major==v.major and @minor==v.minor and @patch<v.patch) or\n"
+        "           (@major==v.major and @minor==v.minor and @patch==v.patch and @deepest<v.deepest)\n"
+        "       end\n"
+        "       def initialize(major=0, minor=0, patch=0, deepest=0)\n"
+        "           @major=major\n"
+        "           @minor=minor\n"
+        "           @patch=patch\n"
+        "           @deepest=deepest\n"
+        "       end\n"
+        "       def to_s\n"
+        "           \"#{major}.#{minor}.#{patch}.#{deepest}\"\n"
+        "       end\n"
+        "   end\n"
+        "end\n"
+    );
 
-	DEC_CONST(INIT_TIMER);
-	DEC_CONST(INIT_AUDIO);
-	DEC_CONST(INIT_VIDEO);
-	DEC_CONST(INIT_CDROM);
-	DEC_CONST(INIT_JOYSTICK);
-	DEC_CONST(INIT_NOPARACHUTE);
-	DEC_CONST(INIT_EVERYTHING);
+    id_begin=rb_intern("begin");
+    id_end=rb_intern("end");
+    id_new=rb_intern("new");
+    id_clone=rb_intern("clone");
 
-	initAudioClasses();
-	initCDClasses();
-	initEventsClasses();
-	initTrueTypeFontClasses();
-	initJoystickClasses();
-	initKeyClasses();
-	initMouseClasses();
-	initTimerClasses();
-	initVideoClasses();
-	initSFontClasses();
-	initFLXClasses();
+    DEC_CONST(INIT_TIMER);
+    DEC_CONST(INIT_AUDIO);
+    DEC_CONST(INIT_VIDEO);
+    DEC_CONST(INIT_CDROM);
+    DEC_CONST(INIT_JOYSTICK);
+    DEC_CONST(INIT_NOPARACHUTE);
+    DEC_CONST(INIT_EVERYTHING);
 
-	initNetClasses();
-	initSDL();
+    initAudioClasses();
+    initCDClasses();
+    initEventsClasses();
+    initTrueTypeFontClasses();
+    initJoystickClasses();
+    initKeyClasses();
+    initMouseClasses();
+    initTimerClasses();
+    initVideoClasses();
+    initSFontClasses();
+    initFLXClasses();
+
+    initNetClasses();
+    initSDL();
 
 }
--- rudl_video.c
+++ rudl_video.c
@@ -6,132 +6,132 @@ ID id_rect, id_atx, id_aty, id_atw, id_ath;
 
 void initVideo()
 {
-	if(!SDL_WasInit(SDL_INIT_VIDEO)){
-		RUDL_ASSERT(!SDL_WasInit(SDL_INIT_AUDIO), "Always use video classes before starting with audio");
-		RUDL_ASSERT(!SDL_WasInit(SDL_INIT_TIMER), "Always use video classes before using the timer");
+    if(!SDL_WasInit(SDL_INIT_VIDEO)){
+        RUDL_ASSERT(!SDL_WasInit(SDL_INIT_AUDIO), "Always use video classes before starting with audio");
+        RUDL_ASSERT(!SDL_WasInit(SDL_INIT_TIMER), "Always use video classes before using the timer");
 
-		DEBUG_S("Starting video subsystem");
-		SDL_VERIFY(!SDL_InitSubSystem(SDL_INIT_VIDEO));
-		SDL_EnableUNICODE(1);
-	}
+        DEBUG_S("Starting video subsystem");
+        SDL_VERIFY(!SDL_InitSubSystem(SDL_INIT_VIDEO));
+        SDL_EnableUNICODE(1);
+    }
 }
 
 void quitVideo()
 {
-	rb_eval_string(
-		"ObjectSpace.each_object(RUDL::Surface) {|x|	\n"
-		"	x.destroy if(x.class==RUDL::Surface)		\n"
-		"}												\n");
-	if(SDL_WasInit(SDL_INIT_VIDEO)){
-		DEBUG_S("Stopping video subsystem");
-		SDL_QuitSubSystem(SDL_INIT_VIDEO);
-	}
+    rb_eval_string(
+        "ObjectSpace.each_object(RUDL::Surface) {|x|    \n"
+        "   x.destroy if(x.class==RUDL::Surface)        \n"
+        "}                                              \n");
+    if(SDL_WasInit(SDL_INIT_VIDEO)){
+        DEBUG_S("Stopping video subsystem");
+        SDL_QuitSubSystem(SDL_INIT_VIDEO);
+    }
 }
 
 __inline__ Uint32 VALUE2COLOR_NOMAP(VALUE colorObject)
 {
-	VALUE r,g,b,a;
-	if(rb_obj_is_kind_of(colorObject, rb_cArray)){
-		switch(RARRAY(colorObject)->len){
-			case 3:
-				r=rb_ary_entry(colorObject, 0);
-				g=rb_ary_entry(colorObject, 1);
-				b=rb_ary_entry(colorObject, 2);
-				return (((Uint32)(NUM2UINT(r)))<<24)+
-					(((Uint32)(NUM2UINT(g)))<<16)+
-					(((Uint32)(NUM2UINT(b)))<<8)+
-					((Uint32)0x000000ff);
-				break;
-			case 4:
-				r=rb_ary_entry(colorObject, 0);
-				g=rb_ary_entry(colorObject, 1);
-				b=rb_ary_entry(colorObject, 2);
-				a=rb_ary_entry(colorObject, 3);
-				return (((Uint32)(NUM2UINT(r)))<<24)+
-					(((Uint32)(NUM2UINT(g)))<<16)+
-					(((Uint32)(NUM2UINT(b)))<<8)+
-					((Uint32)(NUM2UINT(a)));
-				break;
-			default:
-				rb_raise(rb_eTypeError, "Need colorarray with 3 or 4 elements");
-				return Qnil;
-		}
-	}else{
-		return NUM2UINT(colorObject);
-	}
+    VALUE r,g,b,a;
+    if(rb_obj_is_kind_of(colorObject, rb_cArray)){
+        switch(RARRAY(colorObject)->len){
+            case 3:
+                r=rb_ary_entry(colorObject, 0);
+                g=rb_ary_entry(colorObject, 1);
+                b=rb_ary_entry(colorObject, 2);
+                return (((Uint32)(NUM2UINT(r)))<<24)+
+                    (((Uint32)(NUM2UINT(g)))<<16)+
+                    (((Uint32)(NUM2UINT(b)))<<8)+
+                    ((Uint32)0x000000ff);
+                break;
+            case 4:
+                r=rb_ary_entry(colorObject, 0);
+                g=rb_ary_entry(colorObject, 1);
+                b=rb_ary_entry(colorObject, 2);
+                a=rb_ary_entry(colorObject, 3);
+                return (((Uint32)(NUM2UINT(r)))<<24)+
+                    (((Uint32)(NUM2UINT(g)))<<16)+
+                    (((Uint32)(NUM2UINT(b)))<<8)+
+                    ((Uint32)(NUM2UINT(a)));
+                break;
+            default:
+                rb_raise(rb_eTypeError, "Need colorarray with 3 or 4 elements");
+                return Qnil;
+        }
+    }else{
+        return NUM2UINT(colorObject);
+    }
 }
 
 __inline__ Uint32 VALUE2COLOR(VALUE colorObject, SDL_PixelFormat* format)
 {
-	if(rb_obj_is_kind_of(colorObject, rb_cArray)){
-		VALUE r,g,b,a;
-		switch(RARRAY(colorObject)->len){
-			case 3:
-				r=rb_ary_entry(colorObject, 0);
-				g=rb_ary_entry(colorObject, 1);
-				b=rb_ary_entry(colorObject, 2);
-				return SDL_MapRGB(format,
-						(Uint8)NUM2UINT(r),
-						(Uint8)NUM2UINT(g),
-						(Uint8)NUM2UINT(b));
-				break;
-			case 4:
-				r=rb_ary_entry(colorObject, 0);
-				g=rb_ary_entry(colorObject, 1);
-				b=rb_ary_entry(colorObject, 2);
-				a=rb_ary_entry(colorObject, 3);
-				return SDL_MapRGBA(format,
-						(Uint8)NUM2UINT(r),
-						(Uint8)NUM2UINT(g),
-						(Uint8)NUM2UINT(b),
-						(Uint8)NUM2UINT(a));
-				break;
-			default:
-				rb_raise(rb_eTypeError, "Need colorarray with 3 or 4 elements");
-				return Qnil;
-		}
-	}else{
-		Uint32 color=NUM2UINT(colorObject);
-		return SDL_MapRGBA(format, (Uint8)(color>>24), (Uint8)(color>>16&0xff), (Uint8)(color>>8&0xff), (Uint8)(color&0xff));
-	}
+    if(rb_obj_is_kind_of(colorObject, rb_cArray)){
+        VALUE r,g,b,a;
+        switch(RARRAY(colorObject)->len){
+            case 3:
+                r=rb_ary_entry(colorObject, 0);
+                g=rb_ary_entry(colorObject, 1);
+                b=rb_ary_entry(colorObject, 2);
+                return SDL_MapRGB(format,
+                        (Uint8)NUM2UINT(r),
+                        (Uint8)NUM2UINT(g),
+                        (Uint8)NUM2UINT(b));
+                break;
+            case 4:
+                r=rb_ary_entry(colorObject, 0);
+                g=rb_ary_entry(colorObject, 1);
+                b=rb_ary_entry(colorObject, 2);
+                a=rb_ary_entry(colorObject, 3);
+                return SDL_MapRGBA(format,
+                        (Uint8)NUM2UINT(r),
+                        (Uint8)NUM2UINT(g),
+                        (Uint8)NUM2UINT(b),
+                        (Uint8)NUM2UINT(a));
+                break;
+            default:
+                rb_raise(rb_eTypeError, "Need colorarray with 3 or 4 elements");
+                return Qnil;
+        }
+    }else{
+        Uint32 color=NUM2UINT(colorObject);
+        return SDL_MapRGBA(format, (Uint8)(color>>24), (Uint8)(color>>16&0xff), (Uint8)(color>>8&0xff), (Uint8)(color&0xff));
+    }
 }
 
 __inline__ VALUE COLOR2VALUE(Uint32 color, SDL_Surface* surface)
 {
-	Uint8 r,g,b,a;
+    Uint8 r,g,b,a;
 
-	// Is this construction usefull? Should I always use GetRGBA?
-	if(surface->flags&SDL_SRCALPHA){
-		SDL_GetRGBA(color, surface->format, &r, &g, &b, &a);
-		return rb_ary_new3(4, UINT2NUM(r), UINT2NUM(g), UINT2NUM(b), UINT2NUM(a));
-	}else{
-		SDL_GetRGB(color, surface->format, &r, &g, &b);
-		return rb_ary_new3(3, UINT2NUM(r), UINT2NUM(g), UINT2NUM(b));
-	}
+    // Is this construction usefull? Should I always use GetRGBA?
+    if(surface->flags&SDL_SRCALPHA){
+        SDL_GetRGBA(color, surface->format, &r, &g, &b, &a);
+        return rb_ary_new3(4, UINT2NUM(r), UINT2NUM(g), UINT2NUM(b), UINT2NUM(a));
+    }else{
+        SDL_GetRGB(color, surface->format, &r, &g, &b);
+        return rb_ary_new3(3, UINT2NUM(r), UINT2NUM(g), UINT2NUM(b));
+    }
 }
 
 __inline__ void PARAMETER2COORD(VALUE parameter, Sint16* x, Sint16* y)
 {
-	VALUE tmp;
-	if(rb_obj_is_kind_of(parameter, rb_cArray)){
-		tmp=rb_ary_entry(parameter, 0);
-		*x=NUM2Sint16(tmp);
-		tmp=rb_ary_entry(parameter, 1);
-		*y=NUM2Sint16(tmp);
-	}else{
-		rb_raise(rb_eTypeError, "Expected coordinate array with at least 2 elements");
-	}
+    VALUE tmp;
+    if(rb_obj_is_kind_of(parameter, rb_cArray)){
+        tmp=rb_ary_entry(parameter, 0);
+        *x=NUM2Sint16(tmp);
+        tmp=rb_ary_entry(parameter, 1);
+        *y=NUM2Sint16(tmp);
+    }else{
+        rb_raise(rb_eTypeError, "Expected coordinate array with at least 2 elements");
+    }
 }
 
 ///////////////////////////////// INIT
 void initVideoClasses()
 {
-	initVideoSurfaceClasses();
-	initVideoDisplaySurfaceClasses();
-	initVideoRectClasses();
-	initVideoSDLGFXClasses();
-	initVideoBitmaskClasses();
-	
+    initVideoSurfaceClasses();
+    initVideoDisplaySurfaceClasses();
+    initVideoRectClasses();
+    initVideoSDLGFXClasses();
+    initVideoBitmaskClasses();
+
 /*
 =begin
 <<< docs/head
@@ -139,14 +139,14 @@ void initVideoClasses()
 General stuff used by the rest of the video classes.
 == Exceptions
 === SurfacesLostException
-This gruesome thing is thrown bij Surface#blit when Windows manages to destroy all your
+This gruesome thing is thrown by Surface#blit when Windows manages to destroy all your
 surfaces.
 This might happen when switching to another application, for example.
 The only thing to rescue your application is by waiting for blit to stop throwing exceptions,
 then reloading all your surfaces.
 =end */
 
-	classSurfacesLostException=rb_define_class_under(moduleRUDL, "SurfacesLostException", rb_cObject);
+    classSurfacesLostException=rb_define_class_under(moduleRUDL, "SurfacesLostException", rb_cObject);
 /*
 =begin
 == Events
@@ -154,76 +154,76 @@ then reloading all your surfaces.
 --- ResizeEvent#size
 This is [w, h] the new size of the window.
 =end */
-	classResizeEvent=rb_define_class_under(moduleRUDL, "ResizeEvent", classEvent);
-	rb_define_attr(classResizeEvent, "size", 1, 1);
+    classResizeEvent=rb_define_class_under(moduleRUDL, "ResizeEvent", classEvent);
+    rb_define_attr(classResizeEvent, "size", 1, 1);
 /*
 =begin
 === ActiveEvent
 --- ActiveEvent#gain
 --- ActiveEvent#state
 =end */
-	classActiveEvent=rb_define_class_under(moduleRUDL, "ActiveEvent", classEvent);
-	rb_define_attr(classActiveEvent, "gain", 1, 1);
-	rb_define_attr(classActiveEvent, "state", 1, 1);
+    classActiveEvent=rb_define_class_under(moduleRUDL, "ActiveEvent", classEvent);
+    rb_define_attr(classActiveEvent, "gain", 1, 1);
+    rb_define_attr(classActiveEvent, "state", 1, 1);
 /*
 =begin
 === QuitEvent
 This event signals that the user or the program itself has requested to be terminated.
 =end */
-	classQuitEvent=rb_define_class_under(moduleRUDL, "QuitEvent", classEvent);
+    classQuitEvent=rb_define_class_under(moduleRUDL, "QuitEvent", classEvent);
 /*
 =begin
 === VideoExposeEvent
-A VideoExposeEvent event is triggered when the screen has been modified outside of the application, 
+A VideoExposeEvent event is triggered when the screen has been modified outside of the application,
 usually by the window manager and needs to be redrawn.
 =end */
-	classVideoExposeEvent=rb_define_class_under(moduleRUDL, "VideoExposeEvent", classEvent);
+    classVideoExposeEvent=rb_define_class_under(moduleRUDL, "VideoExposeEvent", classEvent);
 /*
 =begin
 == Constants
 === SDL constants:
-SWSURFACE, HWSURFACE, RESIZABLE, ASYNCBLIT, OPENGL, ANYFORMAT, HWPALETTE, DOUBLEBUF, 
+SWSURFACE, HWSURFACE, RESIZABLE, ASYNCBLIT, OPENGL, ANYFORMAT, HWPALETTE, DOUBLEBUF,
 FULLSCREEN, HWACCEL, SRCCOLORKEY, RLEACCELOK, RLEACCEL, SRCALPHA, PREALLOC, NOFRAME
 
 === OpenGL constants:
-GL_RED_SIZE, GL_GREEN_SIZE, GL_BLUE_SIZE, GL_ALPHA_SIZE, GL_BUFFER_SIZE, 
-GL_DOUBLEBUFFER, GL_DEPTH_SIZE, GL_STENCIL_SIZE, GL_ACCUM_RED_SIZE, GL_ACCUM_GREEN_SIZE, 
+GL_RED_SIZE, GL_GREEN_SIZE, GL_BLUE_SIZE, GL_ALPHA_SIZE, GL_BUFFER_SIZE,
+GL_DOUBLEBUFFER, GL_DEPTH_SIZE, GL_STENCIL_SIZE, GL_ACCUM_RED_SIZE, GL_ACCUM_GREEN_SIZE,
 GL_ACCUM_BLUE_SIZE, GL_ACCUM_ALPHA_SIZE
 =end */
 
-	DEC_CONST(SWSURFACE);
-	DEC_CONST(HWSURFACE);
-	DEC_CONST(RESIZABLE);
-	DEC_CONST(ASYNCBLIT);
-	DEC_CONST(OPENGL);
-	DEC_CONST(ANYFORMAT);
-	DEC_CONST(HWPALETTE);
-	DEC_CONST(DOUBLEBUF);
-	DEC_CONST(FULLSCREEN);
-	DEC_CONST(HWACCEL);
-	DEC_CONST(SRCCOLORKEY);
-	DEC_CONST(RLEACCELOK);
-	DEC_CONST(RLEACCEL);
-	DEC_CONST(SRCALPHA);
-	DEC_CONST(PREALLOC);
-	DEC_CONST(NOFRAME);
+    DEC_CONST(SWSURFACE);
+    DEC_CONST(HWSURFACE);
+    DEC_CONST(RESIZABLE);
+    DEC_CONST(ASYNCBLIT);
+    DEC_CONST(OPENGL);
+    DEC_CONST(ANYFORMAT);
+    DEC_CONST(HWPALETTE);
+    DEC_CONST(DOUBLEBUF);
+    DEC_CONST(FULLSCREEN);
+    DEC_CONST(HWACCEL);
+    DEC_CONST(SRCCOLORKEY);
+    DEC_CONST(RLEACCELOK);
+    DEC_CONST(RLEACCEL);
+    DEC_CONST(SRCALPHA);
+    DEC_CONST(PREALLOC);
+    DEC_CONST(NOFRAME);
 
-	DEC_CONST(GL_RED_SIZE);
-	DEC_CONST(GL_GREEN_SIZE);
-	DEC_CONST(GL_BLUE_SIZE);
-	DEC_CONST(GL_ALPHA_SIZE);
-	DEC_CONST(GL_BUFFER_SIZE);
-	DEC_CONST(GL_DOUBLEBUFFER);
-	DEC_CONST(GL_DEPTH_SIZE);
-	DEC_CONST(GL_STENCIL_SIZE);
-	DEC_CONST(GL_ACCUM_RED_SIZE);
-	DEC_CONST(GL_ACCUM_GREEN_SIZE);
-	DEC_CONST(GL_ACCUM_BLUE_SIZE);
-	DEC_CONST(GL_ACCUM_ALPHA_SIZE);
+    DEC_CONST(GL_RED_SIZE);
+    DEC_CONST(GL_GREEN_SIZE);
+    DEC_CONST(GL_BLUE_SIZE);
+    DEC_CONST(GL_ALPHA_SIZE);
+    DEC_CONST(GL_BUFFER_SIZE);
+    DEC_CONST(GL_DOUBLEBUFFER);
+    DEC_CONST(GL_DEPTH_SIZE);
+    DEC_CONST(GL_STENCIL_SIZE);
+    DEC_CONST(GL_ACCUM_RED_SIZE);
+    DEC_CONST(GL_ACCUM_GREEN_SIZE);
+    DEC_CONST(GL_ACCUM_BLUE_SIZE);
+    DEC_CONST(GL_ACCUM_ALPHA_SIZE);
 
-	id_rect=rb_intern("rect");
-	id_atx=rb_intern("@x");
-	id_aty=rb_intern("@y");
-	id_atw=rb_intern("@w");
-	id_ath=rb_intern("@h");
+    id_rect=rb_intern("rect");
+    id_atx=rb_intern("@x");
+    id_aty=rb_intern("@y");
+    id_atw=rb_intern("@w");
+    id_ath=rb_intern("@h");
 }
