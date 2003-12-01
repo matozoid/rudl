78aa9385756480ca763fa7cd87c722102f2172bd
rennex <rennex>
2003-12-01 22:39:10 +0000

    Added ability to supply icon file name for testing

--- samples/work/windowicon.rb
+++ samples/work/windowicon.rb
@@ -3,10 +3,24 @@ require "RUDL"
 include RUDL
 include Constant
 
-icon = Surface.load_new("apple.bmp")
+
+# usage: windowicon.rb [iconfile [transparentcolor]]
+
+colorkey = nil
+
+if ARGV.size > 0
+    iconfile = ARGV[0]
+    colorkey = ARGV[1].hex if ARGV.size > 1
+else
+    iconfile = "apple.bmp"
+    colorkey = 0x0000ff
+end
+
+icon = Surface.load_new(iconfile)
+icon.set_colorkey(colorkey) if colorkey
+
 
 win = DisplaySurface.new [300,200]
-icon.set_colorkey [0,0,255]
 win.set_icon(icon)
 
 win.fill [160,160,160]
