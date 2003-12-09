24b693fb0bcaa4db43a52a347d9eddd60908487d
rennex <rennex>
2003-12-09 15:16:10 +0000

    no message

--- samples/work/scale2x.rb
+++ samples/work/scale2x.rb
@@ -9,16 +9,8 @@ else
     picname = "mslug2-1.png"
 end
 
-origpic = Surface.load_new(picname)
-puts "#{origpic.bitsize}-bit surface: #{picname} #{origpic.w}x#{origpic.h}"
-
-# if it's 24-bit, which isn't supported yet, then turn it to 32-bit.
-if origpic.bytesize == 3
-    pic = Surface.new [origpic.w,origpic.h], SWSURFACE, 32
-    pic.blit origpic, [0,0]
-else
-    pic = origpic
-end
+pic = Surface.load_new(picname)
+puts "#{pic.bitsize}-bit surface: #{picname} #{pic.w}x#{pic.h}"
 
 # open window
 win = DisplaySurface.new [50 + 10*pic.w, 20 + 4*pic.h]
@@ -38,8 +30,9 @@ win.blit quadpic, [40+6*pic.w,10]
 # blit the 3x scaled
 # since there's no scale3x, reduce the 4x one to 75% size B)
 triplepic = quadpic.zoom(0.75, 0.75, true)
+triplepic.unset_colorkey if triplepic.colorkey  # naughty zoom!
 win.blit triplepic, [30+3*pic.w,10]
-win.print [30+3*pic.w, 15+3*pic.h], "(pseudo 3x)", [0,0,0]
+win.print([30+3*pic.w, 15+3*pic.h], "(pseudo 3x)", [0,0,0]) if pic.w*3 >= 78
 
 win.update
 
