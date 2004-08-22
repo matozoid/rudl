ba7d90b550949d357ff3bdf5f7e3e4be105ee1f4
tsuihark <tsuihark>
2004-08-22 22:25:39 +0000

    Moved some samples from the working dir since they seem to be alright.

---
+++ samples/polyscope.rb
@@ -0,0 +1,225 @@
+# Made by Martin Stannard
+
+require 'RUDL' 
+include RUDL
+include Constant
+
+
+$ScreenX = 800
+$ScreenY = 600
+
+$ScreenSize = [$ScreenX, $ScreenY]
+$ScreenXMid = $ScreenX / 2
+$ScreenYMid = $ScreenY / 2
+$Clock = 0.0
+
+class Font
+
+	attr_accessor :image
+	
+	def initialize(filename, size)
+		@font = TrueTypeFont.new(filename, size)
+	end
+	
+	def makeWord(word, colour)
+		@image = @font.render(word, false, colour)
+	end
+end
+
+class PolyElement
+
+	attr_accessor :angle, :dist, :surface
+	
+	def initialize(image)
+		@angle = 0.0
+		@dist = 0.0
+		loadImage(image)
+	end
+	
+	def getXY
+		x = (Math::sin(@angle) * dist) + $ScreenXMid
+		y = (Math::cos(@angle) * dist) + $ScreenYMid
+		[x - @offx, y - @offy]
+	end
+	
+	def blit(surface)
+		surface.blit(@surface , getXY)
+	end
+	
+	def rotate(inc)
+		@angle += inc
+	end
+	
+	def move(inc)
+		@dist += inc
+	end
+	
+	def setColorKey(color)
+		@surface.set_colorkey(color, RLEACCEL)
+	end
+	
+	def loadImage(surface)
+		@surface=surface
+		@offx = @surface.w / 2
+		@offy = @surface.h / 2
+		#@surface.set_ColorKey([0,0,0])
+	end
+	
+end
+   
+class Poly
+
+	def initialize
+		@elements = Array.new
+		@rotspeed = (rand * 0.002)# + 0.01
+		@rotspeed *= -1.0 if rand < 0.5
+		@movespeed = (rand * 5.0) + 2.0
+		@movespeed *= -1.0 if rand < 0.5
+		@rotamp = rand * 5.0 
+		@moveamp = (rand * 8.0) + 1.0
+		newImage
+#		@angleinc = rand 
+#		puts "Poly @rotspeed #{@rotspeed} @movespeed #{@movespeed}"
+	end						
+
+	def addElement
+		@elements.push(PolyElement.new(@surface))
+	end
+	
+	def blit(surface)
+		@elements.each { |e| e.blit(surface) }
+	end
+	
+	def getElement(i)
+		@elements[i]
+	end
+	
+	def addElements(count)
+		startangle = rand * 2 * Math::PI
+		startdist = rand * 300.0
+		
+		count.times do |t|
+			addElement
+			@elements.last.angle = startangle + (Math::PI * 2 / count) * t
+			@elements.last.dist = startdist	
+		end
+	end
+	
+	def rotate
+#   	@elements.each { |e| e.rotate(@rotspeed) }
+		@elements.each do |e|
+#			a = e.angle 
+			e.rotate(Math::sin($Clock * @rotspeed) * @rotamp) 
+		end
+	end
+	
+	def move
+		@elements.each { |e| e.move(Math::sin($Clock * @movespeed) * @moveamp) }
+	end
+	
+	def	update
+		rotate
+		move		
+	end
+	
+	def changeImage
+		
+		@elements.each do |e|
+			e.loadImage(@surface)
+		end
+	end	
+
+	def newImage
+		size=rand(20)+5
+		@surface=Surface.new [size*2, size*2], SWSURFACE|SRCALPHA, 16
+		
+		@surface.filled_circle([size, size], size-1, [rand(255), rand(255), rand(255), rand(255)])
+		@surface.convert_alpha!
+		
+	end
+end
+
+class PolyScope
+
+	def initialize(count)
+		@polys = Array.new
+		count.times do
+			@polys.push(Poly.new)
+			@polys.last.addElements(rand(12) + 1)
+		end
+	end
+	
+	def blit(surface)
+		@polys.each { |p| p.blit(surface) }
+	end
+					
+	def update
+		@polys.each { |p| p.update }
+		add if rand(120) == 0 && @polys.size < 15 
+		remove if rand(100) == 0 && @polys.size > 1 
+		changeImage if rand(200) == 0 
+	end
+	
+	def add
+		@polys.push(Poly.new)
+		@polys.last.addElements(rand(20) + 1)
+	end
+	
+	def remove
+		index = rand(@polys.size)
+		@polys.delete_at(index)
+	end
+	
+	def changeImage	
+		index = rand(@polys.size)
+		@polys[index].changeImage	
+	end	
+end
+
+
+def resetPolys
+	$Polys = PolyScope.new(rand(8) + 2)
+	$Display.fill([0, 0, 0])
+	#$Display.blit($Font.image , [0, 0])
+end
+	
+
+
+
+#$Font = Font.new("goose.ttf", 50)
+#$Font.makeWord("PolyScope", [205, 50, 240])
+$Display=DisplaySurface.new($ScreenSize, HWSURFACE|FULLSCREEN)  #DOUBLEBUF| 
+$Display.fill([0, 0, 0])
+$Display.flip
+
+backImage = Surface.new([$ScreenX, $ScreenY]) 
+#backImage.blit($Font.image , [0, 0])
+
+resetPolys
+#backBuffer=Surface.new($ScreenSize, display.flags)
+#backBuffer.fill([0, 0, 0])
+
+i = 0
+waitflag = true
+
+while true
+#	$Display.fill([0, 0, 0])
+#	$Display.flip
+	$Polys.update
+	$Polys.blit($Display)
+#	$Display.flip
+	if EventQueue.poll.class==KeyDownEvent then
+		keys = Key.pressed?
+		if keys.has_key?(K_SPACE)
+			resetPolys
+			i = 0
+		end
+		waitflag = !waitflag if keys.has_key?(K_w)	   
+		exit if keys.has_key?(K_ESCAPE)
+	end
+	sleep(0.01) if waitflag
+	$Clock += 0.02
+	i += 1
+	#$Display.blit($Font.image , [0, 0])
+	$Display.fill([0, 0, 0]) if rand(1000) == 0
+end
---
+++ samples/sdl_ball.rb
@@ -0,0 +1,130 @@
+# $Log: sdl_ball.rb,v $
+# Revision 1.1  2004/08/22 22:25:41  tsuihark
+# Moved some samples from the working dir since they seem to be alright.
+#
+# Revision 1.1  2003/10/06 22:34:52  tsuihark
+# Converted sdl_ball, as found in libsdl.org's library pages. Odd thing it is.
+#
+
+require 'RUDL'
+include RUDL
+include Constant
+
+$c=32
+$GForce=3.2
+$useps=true
+$usebg=false
+
+class Ball
+	attr_reader :pos
+	attr_reader :velocity, :mass, :accel, :elast, :upforce, :fadecount
+	attr_reader :gfx
+	attr_reader :action
+	
+	def do_action
+		@action.call if @action
+	end
+	
+	def bounce
+		@accel=@velocity*0.33
+		@pos.y-=@accel
+		if @velocity<$GForce
+			@action=proc {fall}
+		else
+			@velocity-=@accel
+		end		
+	end
+	
+	def fall
+		@accel=@velocity*0.33
+		@pos.y+=@accel
+		if @pos.y+@gfx.h >= @display.h
+			@pos.y=@display.h-@gfx.h
+			if @accel>0
+				@action=proc {initbounce}
+			else
+				@action=proc {fadeout}
+			end
+		else
+			@velocity+=@accel
+		end
+	end
+	
+	def fadeout
+		if @fadecount<300
+			if @fadecount%2==0
+				#@gfx=nil
+			else
+				#@gfx=$BallBMP
+			end
+			@fadecount+=1
+		else
+			@action=proc {killball}
+		end
+	end
+	
+	def initbounce
+		@velocity/=1.2
+		@action=proc {bounce}
+	end
+	
+	def initfall
+		@velocity=$GForce-@upforce
+		@action=proc {fall}
+	end
+	
+	def initialize(display, image)
+		@display=display
+		@accel=0
+		@fadecount=0
+		@velocity=0
+		@upforce=0
+		@pos=Mouse.pos
+		@gfx=image
+		@action=proc {initfall}
+	end
+	
+	def paletteswitch
+		if $useps
+			$c+=1
+			@gfx.set_palette(0, [[0,0,0],[$c,$c-16,$c-32]])
+		end
+	end
+	
+	def do_render
+		paletteswitch
+		@display.blit(@gfx, @pos)
+	end
+end
+
+def main
+	doexit=false
+	display=DisplaySurface.new [320,240], FULLSCREEN|HWSURFACE | DOUBLEBUF, 8
+	
+	ball_image=Surface.load_new('media/ball.bmp').convert
+	ball_image.set_colorkey [0,0,0]
+	balls=[]
+	
+	while !doexit
+		EventQueue.get.each do |event|
+			case event
+				when KeyDownEvent
+					case event.key
+						when K_q, K_ESCAPE
+							doexit=true
+							break
+					end
+				when MouseButtonDownEvent
+					balls.push Ball.new(display, ball_image)
+			end
+		end
+		display.fill([0,0,0])
+		balls.each do |ball|
+			ball.do_action
+			ball.do_render
+		end
+		display.flip
+	end
+end
+
+main
